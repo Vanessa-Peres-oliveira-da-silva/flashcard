9ca808986fa5a56c63c22d48e742747fce0525dd
Vanessa-Peres-oliveira-da-silva <160754619+Vanessa-Peres-oliveira-da-silva@users.noreply.github.com>
2024-09-25 17:15:37 -0300

    Projeto cartão de perguntas

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criacartao(
+    'Programação',
+    'O que é CSS.',
+    'O CSS é uma linguagem de estilização.'
+)
+criacartao(
+    'Matemática',
+    'Quantos é 5 X 3 + 4 X 7.',
+    'O resultado é 43.'
+)
+criacartao(
+    'Matemática',
+    'Quantos é 12 elevado ao quadrado.',
+    'O resultado 144.'
+)
+criacartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 225.',
+    'O resultado é 15.'
+
+)
+criacartao(
+    'História',
+    'O que foi o tratado de Versalhes.',
+    ' O tratado de Versalhes encerrou formalmente a Primeira Guerra Mundial.'
+)
+criacartao(
+    'Geografia',
+'O que é desertificação.',
+'É o processo dedegradação de terras áridas, semiáridas e subúmidas.'
+)
